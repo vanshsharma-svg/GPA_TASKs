0f6689c7732309795bd8122eac9bab34ee1c10b7
vanshsharma-svg <agnihotrivansh142@gmail.com>
2026-06-29 02:40:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Google Play Store Analysis")
+
+st.title("📱 Google Play Store Analysis")
+st.write("This project analyzes the Google Play Store dataset using Python.")
+
+st.success("Deployment Successful! 🎉")
